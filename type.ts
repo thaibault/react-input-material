2991d905e4ba414ec6b43117e14831795e0ccd74
torben <t.sickert[~at~]gmail.com>
2020-12-07 19:38:31 +0100

    Automatic update to new version.

--- type.ts
+++ type.ts
@@ -3,7 +3,7 @@
 'use strict'
 /* !
     region header
-    [Project page](https://torben.website/storelocator)
+    [Project page](https://torben.website/react-material-input)
 
     Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
 
