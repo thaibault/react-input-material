037bf645979089663419697b258e2f05a17ff2f5
torben <t.sickert[~at~]gmail.com>
2021-08-04 11:10:27 +0200

    Automatic update to new version.

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,1 +1,7 @@
-node_modules/weboptimizer/declarations.d.ts
+// -*- coding: utf-8 -*-
+/** @module declarations */
+/// <reference path="node_modules/weboptimizer/declarations" />
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
