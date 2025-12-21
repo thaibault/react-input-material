454cdcb0e626580e799f417d99228494ba125852
thaibault <info@torben.website>
2025-12-21 23:16:52 +0100

    Automatic update to new version.

---
+++ end-to-end-tests/test-input.spec.ts
@@ -0,0 +1,18 @@
+// region imports
+import {expect, test} from '@playwright/test'
+// endregion
+test.setTimeout(120_000)
+
+// region accounts
+test(
+    'TODO',
+    async ({page}) => {
+        // given
+
+        // when
+
+        // then
+
+    }
+)
+// endregion
